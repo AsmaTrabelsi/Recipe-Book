d52e3379ed5e5dfd55307b7655dd9ad4b7255acf
asma <asmatra149@gmail.com>
2023-05-19 01:40:23 +0100

    test components with static data

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,7 +2,19 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-footer',
-    templateUrl :'./footer.component.html'
+    templateUrl :'./footer.component.html',
+    styles :[`
+    footer {
+    background-color: #f8f9fa;
+    color: #333;
+    padding: 20px 0;
+    margin-top: 20px;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+  }
+    `]
 })
 export class FooterComponent{
 
